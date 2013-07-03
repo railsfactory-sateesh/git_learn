15ea3c63b9ee01d34e332def6cd0a01372a758d9
sateesh <sateesh@ubuntu.ubuntu-domain>
2013-07-03 07:37:56 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitLearn::Application.config.secret_token = '0016084d0952f5580d121287b33c493f988b2138eb13ec2afa545e1338ecfec44ee437234f1d9c530846d506415424f60110468b14bae750ba5c249c46763b87'
